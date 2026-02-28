4d33a74f46ba78f93816ce1056b991427dca9857
PORNIMA KASLIWAL <jainpornima07@gmail.com>
2026-02-28 09:31:22 +0530

    server complete

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
-const { adminProtect } = require("./middlewares/auth.middlewares.js")
+const {  protect } = require("./middlewares/auth.middlewares.js")
 require("dotenv").config()
 
 const app = express()
@@ -13,7 +13,8 @@ app.use(express.json())
 app.use (cookieParser())
 
 app.use("/api/auth", require("./routes/auth.routes.js"))
-app.use("/api/admin", adminProtect, require("./routes/admin.routes.js"))
+app.use("/api/admin", protect("admin"), require("./routes/admin.routes.js"))
+app.use("/api/employee", protect("employee"), require("./routes/employee.routes.js"))
 
 app.use("/", (req, res) => {
     res.status(404).json({ message: `Resource not found ${req.method} ${req.path}` })
--- server/middlewares/auth.middlewares.js
+++ server/middlewares/auth.middlewares.js
@@ -35,3 +35,37 @@ exports.adminProtect = async (req,res,next) =>{
         res.status(401).json({message:"unable to authenticate"})
     }
 }
+exports.protect = (role) =>  async (req,res,next) =>{ 
+    try {
+        //  check for cookie
+        //                        👇 Cookie NAme from auth.controller.js
+        const TOKEN = req.cookies.TOKEN
+        if(!TOKEN){
+            return res.status(401).json(
+                {message:
+                    process.env.NODE_ENV === PRODUCTION
+                ? "unable to authenticate"
+                  : "No Cookie Found"
+                })
+        }
+        //  validate token
+        jwt.verify(TOKEN,process.env.JWT_KEY,async(err,decode)=>{
+            if(err){
+                return res.status(401).json({message:"invalid token"})
+            }
+
+            const result = await User.findById(decode._id)
+            if(!result){
+                return res.status(401).json({message:"Invalid ID"})
+            }
+            if(result.role !==role){
+                        res.status(403).json({message:"Not Authorized to access this route"})
+            }
+            req.user = decode._id // from auth controller.js(signin)
+            next()
+        })
+        //  validate is admin
+    } catch (error) {
+        res.status(401).json({message:"unable to authenticate"})
+    }
+}
